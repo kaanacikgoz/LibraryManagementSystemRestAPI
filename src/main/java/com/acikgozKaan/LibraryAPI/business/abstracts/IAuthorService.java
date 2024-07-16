fd691cc7f4a66df8ecf03f20e2b4f619813136fd
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-17 02:32:07 +0300

    Added IAuthorService and AuthorManager

--- src/main/java/com/acikgozKaan/LibraryAPI/business/abstracts/IAuthorService.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/business/abstracts/IAuthorService.java
@@ -1,2 +1,11 @@
-package com.acikgozKaan.LibraryAPI.business.abstracts;public interface IAuthorService {
+package com.acikgozKaan.LibraryAPI.business.abstracts;
+
+
+import com.acikgozKaan.LibraryAPI.entity.Author;
+
+public interface IAuthorService {
+
+    Author save(Author author);
+
+
 }
--- src/main/java/com/acikgozKaan/LibraryAPI/business/concretes/AuthorManager.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/business/concretes/AuthorManager.java
@@ -1,2 +1,22 @@
-package com.acikgozKaan.LibraryAPI.business.concretes;public class AuthorManager {
+package com.acikgozKaan.LibraryAPI.business.concretes;
+
+import com.acikgozKaan.LibraryAPI.business.abstracts.IAuthorService;
+import com.acikgozKaan.LibraryAPI.dao.AuthorRepo;
+import com.acikgozKaan.LibraryAPI.entity.Author;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorManager implements IAuthorService {
+
+    private final AuthorRepo authorRepo;
+
+    public AuthorManager(AuthorRepo authorRepo) {
+        this.authorRepo = authorRepo;
+    }
+
+    @Override
+    public Author save(Author author) {
+        return this.authorRepo.save(author);
+    }
+
 }
