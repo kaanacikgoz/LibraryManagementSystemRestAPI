885f78c3f9ca1adf860aab9aa4fd54c3133905cf
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-17 02:37:04 +0300

    Added author save method

--- src/main/java/com/acikgozKaan/LibraryAPI/api/AuthorController.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/api/AuthorController.java
@@ -1,2 +1,25 @@
-package com.acikgozKaan.LibraryAPI.api;public class AuthorController {
+package com.acikgozKaan.LibraryAPI.api;
+
+import com.acikgozKaan.LibraryAPI.business.abstracts.IAuthorService;
+import com.acikgozKaan.LibraryAPI.entity.Author;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/authors")
+public class AuthorController {
+
+    private final IAuthorService authorService;
+
+    public AuthorController(IAuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private void save(@RequestBody Author author) {
+        this.authorService.save(author);
+    }
+
 }
