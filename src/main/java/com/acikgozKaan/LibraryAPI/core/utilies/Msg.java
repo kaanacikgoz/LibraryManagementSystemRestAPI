20314bf2811df9589e9d55fa098a5e860d032c5f
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-17 04:52:02 +0300

    Added ResultHelper For Manage Response Of Controller and Added Constant Msg

--- src/main/java/com/acikgozKaan/LibraryAPI/core/utilies/Msg.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/core/utilies/Msg.java
@@ -1,2 +1,7 @@
-package com.acikgozKaan.LibraryAPI.core.utilies;public class Msg {
+package com.acikgozKaan.LibraryAPI.core.utilies;
+
+public class Msg {
+
+    public static final String CREATED = "Data has been added";
+
 }
--- src/main/java/com/acikgozKaan/LibraryAPI/core/utilies/ResultHelper.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/core/utilies/ResultHelper.java
@@ -1,2 +1,11 @@
-package com.acikgozKaan.LibraryAPI.core.utilies;public class ResultHelper {
+package com.acikgozKaan.LibraryAPI.core.utilies;
+
+import com.acikgozKaan.LibraryAPI.core.result.ResultData;
+
+public class ResultHelper {
+
+    public static <T> ResultData<T> created(T data) {
+        return new ResultData<>(true,Msg.CREATED,"201",data);
+    }
+
 }
