9c695dac50d7705bf4c2b61419b9d3c443154133
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-17 04:47:17 +0300

    Added Result and generic ResultData for customize response

--- src/main/java/com/acikgozKaan/LibraryAPI/core/result/Result.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/core/result/Result.java
@@ -1,2 +1,39 @@
-package com.acikgozKaan.LibraryAPI.core.result;public class Result {
+package com.acikgozKaan.LibraryAPI.core.result;
+
+public class Result {
+
+    private boolean status;
+    private String message;
+    private String code;
+
+    public Result(boolean status, String message, String code) {
+        this.status = status;
+        this.message = message;
+        this.code = code;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
 }
--- src/main/java/com/acikgozKaan/LibraryAPI/core/result/ResultData.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/core/result/ResultData.java
@@ -1,2 +1,16 @@
-package com.acikgozKaan.LibraryAPI.core.result;public class ResultData {
+package com.acikgozKaan.LibraryAPI.core.result;
+
+public class ResultData<T> extends Result {
+
+    private final T data;
+
+    public ResultData(boolean status, String message, String code, T data) {
+        super(status, message, code);
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
 }
