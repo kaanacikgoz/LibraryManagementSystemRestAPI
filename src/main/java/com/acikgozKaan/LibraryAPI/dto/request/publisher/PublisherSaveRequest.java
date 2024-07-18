29a4c9b9e9f5652df18031ff68419b8be94d59ae
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-18 17:22:25 +0300

    Created Publisher DTO (PublisherSaveRequest and PublisherResponse)

--- src/main/java/com/acikgozKaan/LibraryAPI/dto/request/publisher/PublisherSaveRequest.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/dto/request/publisher/PublisherSaveRequest.java
@@ -1,2 +1,50 @@
-package com.acikgozKaan.LibraryAPI.dto.request.publisher;public class PublisherSaveRequest {
+package com.acikgozKaan.LibraryAPI.dto.request.publisher;
+
+import com.acikgozKaan.LibraryAPI.entity.Book;
+
+import java.util.List;
+
+public class PublisherSaveRequest {
+
+    private String name;
+    private int establishmentYear;
+    private String address;
+    private List<Book> bookList;
+
+    public PublisherSaveRequest() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEstablishmentYear() {
+        return establishmentYear;
+    }
+
+    public void setEstablishmentYear(int establishmentYear) {
+        this.establishmentYear = establishmentYear;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
 }
--- src/main/java/com/acikgozKaan/LibraryAPI/dto/response/PublisherResponse.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/dto/response/PublisherResponse.java
@@ -1,2 +1,66 @@
-package com.acikgozKaan.LibraryAPI.dto.response;public class PublisherResponse {
+package com.acikgozKaan.LibraryAPI.dto.response;
+
+import com.acikgozKaan.LibraryAPI.entity.Book;
+
+import java.util.List;
+
+public class PublisherResponse {
+
+    private int id;
+    private String name;
+    private int establishmentYear;
+    private String address;
+    private List<Book> bookList;
+
+    public PublisherResponse() {
+    }
+
+    public PublisherResponse(int id, String name, int establishmentYear, String address, List<Book> bookList) {
+        this.id = id;
+        this.name = name;
+        this.establishmentYear = establishmentYear;
+        this.address = address;
+        this.bookList = bookList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEstablishmentYear() {
+        return establishmentYear;
+    }
+
+    public void setEstablishmentYear(int establishmentYear) {
+        this.establishmentYear = establishmentYear;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
 }
