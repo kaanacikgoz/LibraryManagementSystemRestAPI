d206b18e3637170d4f84bfe52330e90fe85d1145
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-18 18:01:22 +0300

    Created Category DTO Files and Classes

--- src/main/java/com/acikgozKaan/LibraryAPI/dto/request/category/CategorySaveRequest.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/dto/request/category/CategorySaveRequest.java
@@ -1,2 +1,41 @@
-package com.acikgozKaan.LibraryAPI.dto.request.category;public class CategorySaveRequest {
+package com.acikgozKaan.LibraryAPI.dto.request.category;
+
+import com.acikgozKaan.LibraryAPI.entity.Book;
+
+import java.util.List;
+
+public class CategorySaveRequest {
+
+    private String name;
+    private String description;
+    private List<Book> bookList;
+
+    public CategorySaveRequest() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
 }
--- src/main/java/com/acikgozKaan/LibraryAPI/dto/response/CategoryResponse.java
+++ src/main/java/com/acikgozKaan/LibraryAPI/dto/response/CategoryResponse.java
@@ -1,2 +1,57 @@
-package com.acikgozKaan.LibraryAPI.dto.response;public class CategoryResponse {
+package com.acikgozKaan.LibraryAPI.dto.response;
+
+import com.acikgozKaan.LibraryAPI.entity.Book;
+
+import java.util.List;
+
+public class CategoryResponse {
+
+    private int id;
+    private String name;
+    private String description;
+    private List<Book> bookList;
+
+    public CategoryResponse() {
+
+    }
+
+    public CategoryResponse(int id, String name, String description, List<Book> bookList) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.bookList = bookList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
 }
